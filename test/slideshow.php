c3d0e4c8ba3e5d0215bb1872e9f1adc89efe70e5
martianmartin <Martin@Martin-PC.home>
2013-12-16 16:34:08 -0500

    service images

---
+++ test/slideshow.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <div id="show">
+            <div>
+                <img id="1" src="http://farm6.static.flickr.com/5224/5658667829_2bb7d42a9c_m.jpg">
+            </div>
+            <div>
+                <img id="2" src="http://farm6.static.flickr.com/5230/5638093881_a791e4f819_m.jpg">
+            </div>
+            <div>
+                <img id="3" src="http://gillespaquette.ca/images/stack-icon.png">
+            </div>
+        </div>
+    </body>
+</html>
